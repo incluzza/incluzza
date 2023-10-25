eec35e1536216c4949413c5aa7e0df3285d25d98
hkmangla <hemantmangla78@gmail.com>
2023-10-25 20:15:54 +0530

    use crane+oxalica to provide rust dev shell

--- NIX-NOTES.md
+++ NIX-NOTES.md
@@ -3,3 +3,9 @@
 
 1. Add a simple `flake.nix` with a devShell that gets activated *automatically* when you open VSCode.
     - (General step; not rust specific)
+1. **Rust dev shell**: Use https://crane.dev/ to provide a Rust dev shell
+    - We'll also use https://github.com/oxalica/rust-overlay to get the appropriate Rust version based on toolchain
+    - Example, https://github.com/srid/dioxus-desktop-template/blob/master/nix/flake-module.nix
+1. **Rust package**: `nix build` should build the packages
+1. Use treefmt and format Rust sources along with Nix.
+1. Re-factor `flake.nix` by moving Rust logic to a new flake module
--- flake.nix
+++ flake.nix
@@ -3,20 +3,47 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+
+    # Rust inputs
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
       imports = [
       ];
-      perSystem = { config, pkgs, lib, ... }: {
-        devShells.default = pkgs.mkShell {
-          name = "incluzza-dev";
-          nativeBuildInputs = with pkgs; [
-            lazygit
-          ];
+      perSystem = { config, pkgs, lib, system, ... }:
+        let
+          rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile (inputs.self + /Backend/rust-toolchain.toml)).override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+            ];
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+            ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "incluzza-dev";
+            nativeBuildInputs = [
+              pkgs.lazygit
+              rustToolchain
+            ];
+            shellHook = ''
+              # For rust-analyzer 'hover' tooltips to work.
+              export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library";
+            '';
+          };
+          formatter = pkgs.nixpkgs-fmt; # nix fmt (TODO: replace with treefmt)
         };
-        formatter = pkgs.nixpkgs-fmt; # nix fmt
-      };
     };
 }
